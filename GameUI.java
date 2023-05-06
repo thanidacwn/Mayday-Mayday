3261ac8c237e0d79585a7e2b8b4960faa074be18
Napasakorn Boonkerd <89629829+Savetang19@users.noreply.github.com>
2023-05-06 22:19:32 +0700

    start some code ;-;

--- GameUI.java
+++ GameUI.java
@@ -1,3 +1,14 @@
-public class GameUI {
-    
+import Object.*;
+import javax.swing.*;
+
+public class GameUI extends JFrame {
+    private GameObject[] components;
+    private GameLogic game;
+
+    public GameUI(){
+        game = new GameLogic();
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setResizable(false);
+        pack();
+    }
 }
